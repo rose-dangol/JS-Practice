9a6500974b725f7f4fdcb5a4fab498a5d36f66d1
rose <rose.dangol23@gmail.com>
2025-05-13 17:03:07 +0545

    update

--- todo/scripts.js
+++ todo/scripts.js
@@ -1,9 +1,62 @@
-function addTodo(){
+let tasks=[];
+
+function addTask(){
+    let todoName= document.getElementById('taskArea').value;
+    let todoDate=document.getElementById('dateArea').value;
+    if(todoName&&todoDate){
+        tasks.push({name: todoName, date:todoDate});
+        console.log(tasks);
+        displayTasks();
+    }
+}
+
+function displayTasks(){
+    let todoListHTML = ``;
+    for(let i=0;i<tasks.length;i++){
+        // console.log(tasks[i].name);
+        // console.log(tasks[i].date);
+        let taskName=tasks[i].name;
+        let taskDate=tasks[i].date;
+
+        document.querySelector('.todo-section #title').innerHTML=`<p>${taskName}</p>`;
+        document.querySelector('.todo-section #date').innerHTML=`<p>${taskDate}</p>`;
+        document.querySelector('.todo-section #action-button').innerHTML=`<button id="delete" onclick="deleteTodo()">Delete</button>`;
+
+        // const html=`<p>${taskName}</p><br>`;
+        // todoListHTML+=html;
+    }
+}
+function deleteTodo(){
+    console.log("deletee");
+}
+    
+    
+    
+    
+    
+    /*let i,j=0;
+    // let todoList=[['complete calculator', 2025-13-5],['finish todo',2025-13-5]];
     let todoName= document.getElementById('textArea').value;
     let todoDate=document.getElementById('dateArea').value;
-    document.getElementById('title').innerHTML=todoName;
+    let todoListHTML = ``;
+    for(i=0;i<10;i++){
+        while(j!=2){
+            todoList[i,j]=todoName;
+            j++;
+            todoList[i,j]=todoDate;
+            const html=`<p>${todoList}</p><br>`;
+            todoListHTML+=html;
+    
+        }
+    }
+    console.log(todoListHTML);
+
+    /*
+    todoList.push(todoName);
+    console.log(todoList);
+    /*document.getElementById('title').innerHTML=todoName;
     document.getElementById('date').innerHTML=todoDate;
-    document.getElementById('action-button').innerHTML='<button id="delete" onclick="deleteTodo()">Delete</button>';
+    document.getElementById('action-button').innerHTML='<button id="delete" onclick="deleteTodo()">Delete</button>';`
 
 
     `for(let i=0;i<3;i++){
@@ -16,11 +69,7 @@ function addTodo(){
         for (let j = 0; j < todoList[i].length; j++) {
             console.log(todoList[i][j]);
         }
-    }`
+    }*/
 
 //     todoName.value='';
 //     console.log(abc)
-}
-function deleteTodo(){
-
-}
